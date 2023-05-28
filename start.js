12576d6198b9fdab53b1325a091d2f54d25c0410
roshani joshi <joshiroshani1234@gmail.com>
2023-05-28 11:48:11 +0545

    first upload

---
+++ start.js
@@ -0,0 +1,11 @@
+function change(){
+    
+// document.getElementById("hello").innerHTML=("Hello roshani");
+
+document.getElementById("hello").style.color="red";
+
+document.getElementById("hello").style.fontSize="50px";
+}
+function roshani(){
+    
+}
